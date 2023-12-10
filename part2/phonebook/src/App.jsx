edaaf82aa850778f0011bd5eff321de799cbd6a5
fgblackpony <134664813+fedefagundez@users.noreply.github.com>
2023-12-10 11:17:58 -0300

    Refactor

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-1234567" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
   };
 
   const addName = (event) => {
@@ -37,6 +39,10 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
+        <div>
+          filter shown with{" "}
+          <input value={newFilter} onChange={handleFilterChange} />
+        </div>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
