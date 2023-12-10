532b4c5b23398b8b61a28378efe7746b1cb18271
fgblackpony <134664813+fedefagundez@users.noreply.github.com>
2023-12-10 03:05:27 -0300

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas", number: "040-1234567" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const addName = (event) => {
@@ -17,11 +24,13 @@ const App = () => {
         ...persons,
         {
           id: persons.length + 1,
-          name: newName
+          name: newName,
+          number: newNumber
         }
       ]);
     }
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -32,13 +41,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
         <React.Fragment key={person.id}>
-          {person.name}
+          {person.name} {person.number}
           <br />
         </React.Fragment>
       ))}
