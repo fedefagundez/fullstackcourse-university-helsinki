810003140279cab8b595971c9b3beecabcc4410f
fgblackpony <134664813+fedefagundez@users.noreply.github.com>
2023-12-11 08:38:29 -0300

    Filter component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -49,13 +49,23 @@ const App = () => {
         return regexFilter.test(nameLowerCase);
       });
 
+  const Filter = ({ newFilter }) => {
+    return (
+      <div>
+        filter shown with
+        <input
+          autoFocus="autoFocus" //keep the focus on the input
+          value={newFilter}
+          onChange={handleFilterChange}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with{" "}
-        <input value={newFilter} onChange={handleFilterChange} />
-      </div>
+      <Filter newFilter={newFilter} />
       <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
