20c47e2e13405cfbcdb62c340e8bccfb292cef03
fgblackpony <134664813+fedefagundez@users.noreply.github.com>
2023-12-15 12:25:52 -0300

    Exercise 2.11 final

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Filter = ({ newFilter, handleFilter }) => {
   return (
@@ -50,16 +51,23 @@ const Persons = ({personsToShow}) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(
+      response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      }
+    )
+  }
+
+  useEffect(hook, []);
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
