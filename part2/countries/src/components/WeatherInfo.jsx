3f872e086719a9a24281bf78f53deaa82ac2c168
fedefagundez <134664813+fedefagundez@users.noreply.github.com>
2024-06-25 16:42:42 -0300

    Improve error handling for API failures

--- part2/countries/src/components/WeatherInfo.jsx
+++ part2/countries/src/components/WeatherInfo.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const WeatherInfo = ({ capital }) => {
   const [weatherData, setWeatherData] = useState({});
   const apiKey = import.meta.env.VITE_API_KEY;
-  console.log(api_key);
+  console.log(apiKey);
 
   const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${capital}`;
 
@@ -18,19 +18,25 @@ const WeatherInfo = ({ capital }) => {
       .catch((response) => console.log(`Error: ${response}`));
   };
 
-  useEffect(hook);
+  useEffect(hook, []);
 
   return (
     <div>
       <h3>Weather in {capital}</h3>
-      <b>Temperature:</b> {weatherData.current.temperature}ºC <br />
-      <img
-        src={weatherData.current.weather_icons[0]}
-        alt={weatherData.current.weather_descriptions[0]}
-      />{" "}
-      <br />
-      <b>Wind:</b> {weatherData.current.wind_speed} mph direction{" "}
-      {weatherData.current.wind_dir}
+      {weatherData.current ? (
+        <>
+          <b>Temperature:</b> {weatherData.current.temperature}ºC <br />
+          <img
+            src={weatherData.current.weather_icons[0]}
+            alt={weatherData.current.weather_descriptions[0]}
+          />{" "}
+          <br />
+          <b>Wind:</b> {weatherData.current.wind_speed} mph direction{" "}
+          {weatherData.current.wind_dir}
+        </>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
     </div>
   );
 };
